dfa68533d0c27a2092489c0750fbe03d405caf1f
omchavan <omrajendrachavan@gmail.com>
2025-12-27 16:43:00 +0530

    WIP:half wrok is completed

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import io
 import csv
 from flask import Flask , render_template , redirect ,url_for,request , Response
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager,UserMixin,login_user,current_user,login_required
+from flask_login import LoginManager,UserMixin,login_user,logout_user, current_user,login_required
 from werkzeug.security import generate_password_hash,check_password_hash
 
 app =Flask(__name__)
@@ -18,7 +18,7 @@ login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.querry.get(int(user_id))
+    return User.query.get(int(user_id))
 
 
 # Database Models
@@ -28,7 +28,7 @@ class User(UserMixin,db.Model):
     password = db.Column(db.String(200),nullable=False)
     role = db.Column(db.String(20),default ='staff')   #admin or staff
 
-class Product(db.Model):
+class Employee(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     full_name = db.Column(db.String(100),nullable = False)
     department = db.Column(db.String(100))
@@ -75,7 +75,7 @@ def add_employee():
 @app.route('/export_staff')
 @login_required
 def export_staff():
-    employees = Employees.query.all()
+    employees = Employee.query.all()
     output = io.StringIO()
     writer = csv.writer(output)
     writer.writerow(['ID','Name','Department','Position','Email'])  
@@ -93,6 +93,20 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
+
+@app.route('/delete/<int:emp_id>')
+@login_required
+def delete_employee(emp_id):
+    employee = Employee.query.get_or_404(emp_id)
+
+    # Optional: Admin-only protection
+    if current_user.role != 'admin':
+        return redirect(url_for('dashboard'))
+
+    db.session.delete(employee)
+    db.session.commit()
+
+    return redirect(url_for('dashboard'))
 
 #  Initialize System
 with app.app_context():
